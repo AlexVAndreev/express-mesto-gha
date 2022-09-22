23d56111521ebad22e0e9be47eeb0439d1e8c5be
Alexandr Andreev <awa3awa@yandex.ru>
2022-09-22 18:02:17 +0700

    Sew

--- controllers/cards.js
+++ controllers/cards.js
@@ -1,47 +1,67 @@
 const Card = require('../models/card');
+const { INPUT_ERROR, NOT_FOUND_ERROR, DEFAULT_ERROR } = require('../utils/const');
 
-module.exports.getCards = (req, res, next) => {
+module.exports.getCards = (req, res) => {
   Card.find({})
     .then((cards) => res.send(cards))
-    .catch(next);
+    .catch(() => {
+      res.status(DEFAULT_ERROR).send({ message: 'Ошибка на сервер' });
+    });
 };
 
-module.exports.createCard = (req, res, next) => {
+module.exports.createCard = (req, res) => {
   const { name, link } = req.body;
   const owner = req.user._id;
   Card.create({ name, link, owner })
     .then((card) => res.send(card))
     .catch((err) => {
-      console.log(`Хренька: ${err.message}`);
-      next();
+      if (err.name === 'ValidationError') {
+        res.status(INPUT_ERROR).send({ message: 'Переданы некорректные данные' });
+      } else res.status(DEFAULT_ERROR).send({ message: 'Ошибка насервере' });
     });
 };
 
-module.exports.deleteCard = (req, res, next) => {
+module.exports.deleteCard = (req, res) => {
   Card.findByIdAndRemove(req.params.cardId)
     .then((card) => res.send(card))
-    .catch(next);
+    .catch((err) => {
+      if (err.statusCode === 404) {
+        res.status(NOT_FOUND_ERROR).send({ message: err.message });
+      } else if (err.name === 'CastError') {
+        res.status(INPUT_ERROR).send({ message: 'Ошибка _id карточки' });
+      } else res.status(NOT_FOUND_ERROR).send({ message: err.message });
+    });
 };
-module.exports.likeCard = (req, res, next) => Card.findByIdAndUpdate(
+module.exports.likeCard = (req, res) => Card.findByIdAndUpdate(
   req.params.cardId,
   { $addToSet: { likes: req.user._id } }, // добавить _id в массив, если его там нет
   { new: true },
-// eslint-disable-next-line consistent-return
-).then((card) => {
-  if (card) {
-    return res.send(card);
-  }
-})
-  .catch(next);
+).orFail(() => {
+  const error = new Error('Передан несуществующий _id карточки');
+  error.statusCode = 404;
+  throw error;
+}).then((card) => res.send({ data: card }))
+  .catch((err) => {
+    if (err.statusCode === 404) {
+      res.status(NOT_FOUND_ERROR).send({ message: err.message });
+    } else if (err.name === 'ValidationError' || err.name === 'CastError') {
+      res.status(INPUT_ERROR).send({ message: 'Проблемы с Лайком' });
+    } else res.status(DEFAULT_ERROR).send({ message: 'Ошибка на стороне сервера' });
+  });
 
-module.exports.dislikeCard = (req, res, next) => Card.findByIdAndUpdate(
+module.exports.dislikeCard = (req, res) => Card.findByIdAndUpdate(
   req.params.cardId,
   { $pull: { likes: req.user._id } }, // убрать _id из массива
   { new: true },
-// eslint-disable-next-line consistent-return
-).then((card) => {
-  if (card) {
-    return res.send(card);
-  }
-})
-  .catch(next);
+).orFail(() => {
+  const error = new Error('Передан несуществующий _id карточки');
+  error.statusCode = 404;
+  throw error;
+}).then((card) => res.send({ data: card }))
+  .catch((err) => {
+    if (err.statusCode === 404) {
+      res.status(NOT_FOUND_ERROR).send({ message: err.message });
+    } else if (err.name === 'ValidationError' || err.name === 'CastError') {
+      res.status(INPUT_ERROR).send({ message: 'Проблемы с Лайком' });
+    } else res.status(DEFAULT_ERROR).send({ message: 'Ошибка на стороне сервера' });
+  });
--- controllers/users.js
+++ controllers/users.js
@@ -1,4 +1,5 @@
 const User = require('../models/user');
+const { INPUT_ERROR, NOT_FOUND_ERROR, DEFAULT_ERROR } = require('../utils/const');
 
 module.exports.getUser = (req, res) => {
   User.findById(req.params.userId)
@@ -8,7 +9,13 @@ module.exports.getUser = (req, res) => {
       }
       return res.send(user);
     })
-    .catch((err) => err.status);
+    .catch((err) => {
+      if (err.statusCode === 404) {
+        res.status(NOT_FOUND_ERROR).send({ message: err.message });
+      } else if (err.name === 'CastError') {
+        res.status(INPUT_ERROR).send({ message: 'некорректный _id профиля' });
+      } else res.status(DEFAULT_ERROR).send({ message: 'Ошибка сервера' });
+    });
 };
 
 module.exports.getUsers = (req, res) => {
@@ -22,30 +29,46 @@ module.exports.postUsers = (req, res) => {
   // вернём записанные в базу данные
     .then((user) => res.send({ data: user }))
   // данные не записались, вернём ошибку
-    .catch((err) => err.status);
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        res.status(INPUT_ERROR).send({ message: 'Ошибка' });
+      } else res.status(DEFAULT_ERROR).send({ message: 'Ошибка сервера' });
+    });
 };
 module.exports.updateProfile = (req, res) => {
   const { name, about } = req.body;
 
   User.findByIdAndUpdate(req.user._id, { name, about })
-    .then((user) => res.send({
-      _id: user._id,
-      avatar: user.avatar,
-      name,
-      about,
-    }))
-    .catch((err) => err.status);
+    .orFail(() => {
+      const error = new Error('Пользователь не найден');
+      error.statusCode = 404;
+      throw error;
+    })
+    .then((user) => res.send({ data: user }))
+    .catch((err) => {
+      if (err.statusCode === 404) {
+        res.status(NOT_FOUND_ERROR).send({ message: 'Пользователь не найден' });
+      } else if (err.name === 'ValidationError' || err.name === 'CastError') {
+        res.status(INPUT_ERROR).send({ message: 'некорректные данные при обновлении профиля' });
+      } else res.status(DEFAULT_ERROR).send({ message: 'Ошибка сервера' });
+    });
 };
 
 module.exports.updateAvatar = (req, res) => {
   const { avatar } = req.body;
 
   User.findByIdAndUpdate(req.user._id, { avatar })
-    .then((user) => res.send({
-      _id: user._id,
-      avatar,
-      name: user.name,
-      about: user.about,
-    }))
-    .catch((err) => err.status);
+    .orFail(() => {
+      const error = new Error('Пользователь не найден');
+      error.statusCode = 404;
+      throw error;
+    })
+    .then((user) => res.send({ data: user }))
+    .catch((err) => {
+      if (err.statusCode === 404) {
+        res.status(NOT_FOUND_ERROR).send({ message: err.message });
+      } else if (err.name === 'ValidationError' || err.name === 'CastError') {
+        res.status(INPUT_ERROR).send({ message: 'некорректные данные при обновлении профиля' });
+      } else res.status(DEFAULT_ERROR).send({ message: 'Ошибка сервера' });
+    });
 };
