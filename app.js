3f3dbdf4c963ca6226d1d7c4c4148a0e110b6fc5
Alexandr Andreev <awa3awa@yandex.ru>
2022-09-22 18:49:43 +0700

    Sewi

--- app.js
+++ app.js
@@ -8,34 +8,6 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-const allowedCors = [
-  'https://praktikum.tk',
-  'http://praktikum.tk',
-  'localhost:3000',
-  'http://localhost:3000',
-  'http://127.0.0.1:3000',
-];
-
-// eslint-disable-next-line consistent-return
-app.use((req, res, next) => {
-  const { origin } = req.headers;
-
-  if (allowedCors.some((e) => e.test && e.test(origin)) || allowedCors.includes(origin)) {
-    res.header('Access-Control-Allow-Origin', origin);
-    res.header('Access-Control-Allow-Credentials', true);
-  }
-  const { method } = req;
-  const requestHeaders = req.headers['access-control-request-headers'];
-  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
-
-  if (method === 'OPTIONS') {
-    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
-    res.header('Access-Control-Allow-Headers', requestHeaders);
-    return res.end();
-  }
-
-  next();
-});
 app.use((req, res, next) => {
   req.user = {
     _id: '632c1affe6cd5f768837bde5', // вставьте сюда _id созданного в предыдущем пункте пользователя
