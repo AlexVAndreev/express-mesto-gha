343ebd06825e483798a457b54d022f7b7979dfd2
Alexandr Andreev <awa3awa@yandex.ru>
2022-09-22 16:25:01 +0700

    Sewi Pew Pew

--- controllers/cards.js
+++ controllers/cards.js
@@ -26,10 +26,22 @@ module.exports.likeCard = (req, res) => Card.findByIdAndUpdate(
   req.params.cardId,
   { $addToSet: { likes: req.user._id } }, // добавить _id в массив, если его там нет
   { new: true },
-);
+// eslint-disable-next-line consistent-return
+).then((card) => {
+  if (card) {
+    return res.send(card);
+  }
+})
+  .catch(console.log('alarm!! card not disliked!'));
 
 module.exports.dislikeCard = (req, res) => Card.findByIdAndUpdate(
   req.params.cardId,
   { $pull: { likes: req.user._id } }, // убрать _id из массива
   { new: true },
-);
+// eslint-disable-next-line consistent-return
+).then((card) => {
+  if (card) {
+    return res.send(card);
+  }
+})
+  .catch(console.log('alarm!! card not disliked!'));
