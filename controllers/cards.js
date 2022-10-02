57afe538ba3d88de18d4ce01cf171243d6253eb0
Alexandr Andreev <awa3awa@yandex.ru>
2022-10-02 12:12:43 +0700

    ыыы

--- controllers/cards.js
+++ controllers/cards.js
@@ -1,4 +1,6 @@
 const Card = require('../models/card');
+const NotFoundError = require('../errors/NotFoundError');
+const ForbiddenError = require('../errors/ForbiddenError');
 const { INPUT_ERROR, NOT_FOUND_ERROR, DEFAULT_ERROR } = require('../utils/const');
 
 module.exports.getCards = (req, res) => {
@@ -21,20 +23,22 @@ module.exports.createCard = (req, res) => {
     });
 };
 
-module.exports.deleteCard = (req, res) => {
-  Card.findByIdAndRemove(req.params.cardId).orFail(() => {
-    const error = new Error('Карточка с указанным _id не найдена');
-    error.statusCode = 404;
-    throw error;
-  })
-    .then((card) => res.send(card))
-    .catch((err) => {
-      if (err.statusCode === 404) {
-        res.status(NOT_FOUND_ERROR).send({ message: err.message });
-      } else if (err.name === 'CastError') {
-        res.status(INPUT_ERROR).send({ message: 'Ошибка _id карточки' });
-      } else res.status(NOT_FOUND_ERROR).send({ message: err.message });
-    });
+module.exports.deleteCard = (req, res, next) => {
+  const removeCard = () => {
+    Card.findByIdAndRemove(req.params.cardId)
+      .then((card) => res.send(card))
+      .catch(next);
+  };
+
+  Card.findById(req.params.cardId)
+    .then((card) => {
+      if (!card) next(new NotFoundError('Карточки не существует'));
+      if (req.user._id === card.owner.toString()) {
+        return removeCard();
+      }
+      return next(new ForbiddenError('Нельзя удалять чужую карточку'));
+    })
+    .catch(next);
 };
 module.exports.likeCard = (req, res) => Card.findByIdAndUpdate(
   req.params.cardId,
