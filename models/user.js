d779721da0b00e5eae581e7c0c0b28f49c4daec4
Alexandr Andreev <awa3awa@yandex.ru>
2022-10-02 22:49:54 +0700

    Sewi

--- models/user.js
+++ models/user.js
@@ -2,7 +2,6 @@ const mongoose = require('mongoose');
 const isUrl = require('validator/lib/isURL');
 const isEmail = require('validator/lib/isEmail');
 const bcrypt = require('bcryptjs');
-const UnauthorizedError = require('../errors/UnauthorizedError');
 
 const userSchema = new mongoose.Schema(
   {
@@ -43,24 +42,23 @@ const userSchema = new mongoose.Schema(
     },
   },
 );
+
 // eslint-disable-next-line func-names
 userSchema.statics.findUserByCredentials = function (email, password) {
-  return this.findOne({ email }, { runValidators: true })
-    .select('+password')
+  return this.findOne({ email }).select('+password')
     .then((user) => {
       if (!user) {
-        return Promise.reject(
-          new UnauthorizedError('Неправильные почта или пароль'),
-        );
+        return Promise.reject(new Error('Неправильные почта или пароль'));
       }
-      return bcrypt.compare(password, user.password).then((matched) => {
-        if (!matched) {
-          return Promise.reject(
-            new UnauthorizedError('Неправильные почта или пароль'),
-          );
-        }
-        return user;
-      });
+
+      return bcrypt.compare(password, user.password)
+        .then((matched) => {
+          if (!matched) {
+            return Promise.reject(new Error('Неправильные почта или пароль'));
+          }
+          return user;
+        });
     });
 };
+
 module.exports = mongoose.model('User', userSchema);
