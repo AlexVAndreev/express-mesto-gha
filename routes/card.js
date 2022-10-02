e3816b0b6a95d2e10f84e378f245b9fa9ff32fb3
Alexandr Andreev <awa3awa@yandex.ru>
2022-10-02 20:13:39 +0700

    sewi

--- routes/card.js
+++ routes/card.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const { celebrate, Joi } = require('celebrate');
 const {
   getCards,
   createCard,
@@ -7,8 +8,19 @@ const {
   dislikeCard,
 } = require('../controllers/cards');
 
+const regExpLink = /(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})/;
+
 router.get('/', getCards);
-router.post('/', createCard);
+router.post(
+  '/',
+  celebrate({
+    body: Joi.object().keys({
+      name: Joi.string().required().min(2).max(30),
+      link: Joi.string().required().pattern(regExpLink),
+    }),
+  }),
+  createCard,
+);
 router.delete('/:cardId', deleteCard);
 router.put('/:cardId/likes', likeCard);
 router.delete('/:cardId/likes', dislikeCard);
