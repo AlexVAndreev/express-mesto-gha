20030ee1f52b90b33c4e788c067be41771959273
Alexandr Andreev <awa3awa@yandex.ru>
2022-10-03 10:05:40 +0700

    Sew

--- app.js
+++ app.js
@@ -1,24 +1,20 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-const {
-  celebrate, Joi, errors,
-} = require('celebrate');
+const { celebrate, Joi, errors } = require('celebrate');
 const cookieParser = require('cookie-parser');
 const auth = require('./middlewares/auth');
 
 const { PORT = 3000 } = process.env;
-const app = express();
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cookieParser());
-
 const {
   login,
   createUser,
 } = require('./controllers/users');
 
+const app = express();
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: true }));
 mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {
   useNewUrlParser: true,
 });
@@ -45,10 +41,10 @@ app.post('/signup', celebrate({
   }),
 }), createUser);
 
-app.use(errors());
 app.use('/users', auth, require('./routes/user'));
 app.use('/cards', auth, require('./routes/card'));
 
+app.use(errors());
 app.use((err, req, res, next) => {
   const { statusCode = 500, message } = err;
   res.status(statusCode).send({ message: statusCode === 500 ? 'Ошибка сервера' : message });
--- routes/user.js
+++ routes/user.js
@@ -12,21 +12,26 @@ const {
 const regExp = /(https?:\/\/)([www.]?[a-zA-Z0-9-]+\.)([^\s]{2,})/;
 
 router.get('/', getUsers);
+
 router.get('/:userId', celebrate({
   params: Joi.object().keys({
     userId: Joi.string().required().hex(),
   }),
 }), getUser);
+
 router.get('/me', getMe);
+
 router.patch('/me', celebrate({
   body: Joi.object().keys({
     name: Joi.string().min(2).max(30),
     about: Joi.string().min(2).max(30),
   }),
 }), updateProfile);
+
 router.patch('/me/avatar', celebrate({
   body: Joi.object().keys({
     avatar: Joi.string().required().pattern(regExp),
   }),
 }), updateAvatar);
+
 module.exports = router;
