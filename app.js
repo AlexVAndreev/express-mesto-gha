22a65933c208f9f6972b86cf0ff2e054ebcf08a9
Alexandr Andreev <awa3awa@yandex.ru>
2022-10-16 20:14:05 +0700

    Sewi

--- app.js
+++ app.js
@@ -24,33 +24,30 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // CORS
 
 const allowedCors = [
-  'https://praktikum.tk',
-  'http://praktikum.tk',
   'localhost:3000',
   'http://localhost:3000',
-  'http://127.0.0.1:3000',
-  /(https|http)?:\/\/(?:www\.|(?!www))awa.nomoredomains.icu\/[a-z]+\/|[a-z]+\/|[a-z]+(\/|)/,
+  'https://awa.nomoredomains.icu/',
+  'http://awa.nomoredomains.icu/',
 ];
 
-// eslint-disable-next-line consistent-return
 app.use((req, res, next) => {
-  const { origin } = req.headers;
+  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
 
-  if (allowedCors.some((e) => e.test && e.test(origin)) || allowedCors.includes(origin)) {
-    res.header('Access-Control-Allow-Origin', origin);
-    res.header('Access-Control-Allow-Credentials', true);
-  }
+  const { origin } = req.headers;
   const { method } = req;
   const requestHeaders = req.headers['access-control-request-headers'];
-  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+
+  if (allowedCors.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+  }
 
   if (method === 'OPTIONS') {
     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
     res.header('Access-Control-Allow-Headers', requestHeaders);
-    return res.end();
+    return res.status(200).send();
   }
 
-  next();
+  return next();
 });
 
 mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {
